406f764647504761868160762ae53fa50a69f94d
andrzej-herman <aherman@san.edu.pl>
2023-11-17 12:47:20 +0100

    Add project

---
+++ QuizApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using QuizApp.backend;
+
+Console.WriteLine("Hello");
+
+
+int x;
+string z = "";
+Question pytanie1 = new Question();
+pytanie1.Category = 100;
+pytanie1.Content = "Jak miał na imię Einstein?";
+pytanie1.Answer_01 = "Albert";
+
+
+Console.WriteLine(pytanie1.Content);
+pytanie1 = null;
+Console.WriteLine(pytanie1.Content);
+
+
+Console.ReadLine();
